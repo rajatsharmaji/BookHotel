6e022f1df1198a59a5f8e04d39d3afacd3fd5a34
Rajat Sharma <rajjusharmarajat@gmail.com>
2024-02-11 11:58:05 +0530

    appdend and remove child function added

--- index.js
+++ index.js
@@ -19,10 +19,22 @@ adultMinus.addEventListener("click", function () {
 childPlus.addEventListener("click", function () {
   childValue.value = parseInt(childValue.value) + 1;
   totalValue.value = parseInt(adultValue.value) + parseInt(childValue.value);
+  const selectElement = document.createElement("select");
+  selectElement.className = "ageselect";
+  selectElement.innerHTML =
+    "<option value>age</option><option value=1>1</option><option value=2>2</option><option value=3>3</option><option value=4>4</option><option value=5>5</option><option value=6>6</option><option value=7>7</option><option value=8>8</option><option value=9>9</option><option value=10>10</option><option value=11>11</option><option value=12>12</option>";
+  const childDiv = document.getElementById("child-div");
+  childDiv.appendChild(selectElement);
 });
+
 childMinus.addEventListener("click", function () {
   if (parseInt(childValue.value) > 1) {
     childValue.value = parseInt(childValue.value) - 1;
     totalValue.value = parseInt(adultValue.value) + parseInt(childValue.value);
+    const childDiv = document.getElementById("child-div");
+    const selectElements = childDiv.getElementsByClassName("ageselect");
+    if (selectElements.length > 0) {
+      childDiv.removeChild(selectElements[selectElements.length - 1]);
+    }
   }
 });
